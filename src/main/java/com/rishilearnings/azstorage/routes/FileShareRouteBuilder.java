b4145c8fefa7c28fabbf1a09f20d67767e0b9e45
sabtharishi <sabtharishi2004@gmail.com>
2024-12-23 08:45:55 -0500

    Azure file share and blob example with Apace Camel

--- src/main/java/com/rishilearnings/azstorage/routes/FileShareRouteBuilder.java
+++ src/main/java/com/rishilearnings/azstorage/routes/FileShareRouteBuilder.java
@@ -15,24 +15,19 @@ public class FileShareRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
 
 
-//        from("azure-files://rishifilestorage/fuse/payload?move=.done&credentialType=AZURE_SAS&sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-01-03T20:23:12Z&st=2024-12-23T12:23:12Z&spr=https&sig=5sjyQAW72qYqjFsWKk17xzR45ZJHd0axN5hySas7RXA%3D")
-//                .log("new File found:: ${body}");
-
-
-
         // triggered when a new file is placed to given folder (not inside its subfolder)
 
-        from("azure-files://rishifilestorage/fuse/payload?move=.done&credentialType=SHARED_ACCOUNT_KEY&sharedKey=5ah278OtEEXSTINjQZVB5sdd68biJEeTTFMseVs46UO1jbRNvAfL5NS0K3akJhH2hVGHqDjT/3ud+ASthBPedw==")
+        from("azure-files://rishifilestorage/fuse/payload?move=.done&credentialType=SHARED_ACCOUNT_KEY&sharedKey=")
                 .log("new File found:: ${body}");
 
 
         // triggered when a new file is placed to given folder (not inside its subfolder)
 
-        from("azure-files://rishifilestorage/fuse/payload/claims?move=.done&credentialType=SHARED_ACCOUNT_KEY&sharedKey=5ah278OtEEXSTINjQZVB5sdd68biJEeTTFMseVs46UO1jbRNvAfL5NS0K3akJhH2hVGHqDjT/3ud+ASthBPedw==")
+        from("azure-files://rishifilestorage/fuse/payload/claims?move=.done&credentialType=SHARED_ACCOUNT_KEY&sharedKey=")
                 .log("new File under claims folder found:: ${body}")
                         .setHeader(FilesHeaders.FILE_NAME, constant("from_customer_1.xml"))
                 .setProperty("transactionId", UUID::randomUUID)
-                .toD("azure-files://rishifilestorage/fuse/dump/${exchangeProperty.transactionId}?credentialType=SHARED_ACCOUNT_KEY&sharedKey=5ah278OtEEXSTINjQZVB5sdd68biJEeTTFMseVs46UO1jbRNvAfL5NS0K3akJhH2hVGHqDjT/3ud+ASthBPedw==")
+                .toD("azure-files://rishifilestorage/fuse/dump/${exchangeProperty.transactionId}?credentialType=SHARED_ACCOUNT_KEY&sharedKey=")
         ;
 
 
